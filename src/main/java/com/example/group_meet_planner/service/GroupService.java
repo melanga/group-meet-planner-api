8ba86405190675d0347978e2a75fa8b2576d8969
Melanga Dissanayake <52516959+melanga@users.noreply.github.com>
2023-01-20 16:25:54 +0530

    added functionality to add and remove members from a group

--- src/main/java/com/example/group_meet_planner/service/GroupService.java
+++ src/main/java/com/example/group_meet_planner/service/GroupService.java
@@ -1,7 +1,9 @@
 package com.example.group_meet_planner.service;
 
 import com.example.group_meet_planner.dto.GroupDTO;
+import com.example.group_meet_planner.entity.AppUser;
 import com.example.group_meet_planner.entity.Group;
+import com.example.group_meet_planner.repository.AppUserRepository;
 import com.example.group_meet_planner.repository.GroupRepository;
 import org.springframework.stereotype.Service;
 
@@ -10,9 +12,11 @@ import java.util.List;
 @Service
 public class GroupService {
     private final GroupRepository groupRepository;
+    private final AppUserRepository appUserRepository;
 
-    public GroupService(GroupRepository groupRepository) {
+    public GroupService(GroupRepository groupRepository, AppUserRepository appUserRepository) {
         this.groupRepository = groupRepository;
+        this.appUserRepository = appUserRepository;
     }
 
     public GroupDTO createGroup(Group group) {
@@ -65,5 +69,35 @@ public class GroupService {
             return joinedGroups.stream().map(GroupDTO::new).toList();
         }
         return null;
+    }
+
+    public String addUserToGroup(String groupId, String username) {
+        Group group = groupRepository.findGroupById(groupId);
+        AppUser user = appUserRepository.findByUsername(username);
+        if (group != null && user != null) {
+            if (group.getMembers().contains(user) || group.getCreatedBy().equals(user)) {
+                return "User already in group";
+            }
+            group.getMembers().add(user);
+            groupRepository.save(group);
+            return "User '" + username + "' added to group successfully";
+        } else {
+            return null;
+        }
+    }
+
+    public String removeUserFromGroup(String groupId, String username) {
+        Group group = groupRepository.findGroupById(groupId);
+        AppUser user = appUserRepository.findByUsername(username);
+        if (group != null && user != null) {
+            if (group.getCreatedBy().equals(user)) {
+                return "Cannot remove owner of group";
+            }
+            group.getMembers().remove(user);
+            groupRepository.save(group);
+            return "User '" + username + "' removed from group successfully";
+        } else {
+            return null;
+        }
     }
 }
